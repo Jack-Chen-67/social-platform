adb28538b22609a7a53fd9690b6ba16d04bbee58
itcjx <hello@itcjx.cn>
2025-07-13 16:18:19 +0800

    初始化项目

---
+++ src/test/java/com/itcjx/socialplatform/SocialPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcjx.socialplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
